8fa2101ec7d109cd856a31dd8a02de7fba89349f
mmcachran <mattmcachran@gmail.com>
2018-06-07 08:51:18 -0400

    Adds composer config and adds helpers base file.

--- ep-rules-builder.php
+++ ep-rules-builder.php
@@ -4,8 +4,17 @@
  * Description: Control (boost, bury, hide) search results based on seach keywords.
  * Version:     0.1
  * Author:      mmcachran
- * Author URI:  https://github.com/mmcachran
+ * Author URI:  https://github.com/mmcachran/ElasticPress-Rules-Builder
  * License:     GPLv2 or later
  * Text Domain: ep-rules-builder
  * Domain Path: /lang/
+ *
+ * @package ElasticPress Rules Builder
  */
+
+namespace EP_Rules_Builder;
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
---
+++ includes/EP_Rules_Builder/Helpers.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Core file to include helpers.
+ *
+ * @package ElasticPress Rules Builder
+ */
+
+namespace EP_Rules_Builder;
+
