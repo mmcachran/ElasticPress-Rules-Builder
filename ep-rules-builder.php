a85160489ad43ebd763594c9f6987bb83ada59cd
mmcachran <mattmcachran@gmail.com>
2018-06-07 08:42:47 -0400

    Adds base plugin file.

---
+++ ep-rules-builder.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: ElasticPress Rules Builder
+ * Description: Control (boost, bury, hide) search results based on seach keywords.
+ * Version:     0.1
+ * Author:      mmcachran
+ * Author URI:  https://github.com/mmcachran
+ * License:     GPLv2 or later
+ * Text Domain: ep-rules-builder
+ * Domain Path: /lang/
+ */
