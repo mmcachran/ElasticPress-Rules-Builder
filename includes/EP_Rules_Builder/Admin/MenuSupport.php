5c6fe2ba7e8e930ab19fdf321fbdcc5021192b81
mmcachran <mattmcachran@gmail.com>
2018-06-08 21:09:20 -0400

    Add menu support for plugin.

---
+++ includes/EP_Rules_Builder/Admin/MenuSupport.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Adds menu support for plugin.
+ *
+ * @package ElasticPress Rules Builder
+ */
+
+namespace EP_Rules_Builder\Admin;
+
+/**
+ * Class for adding menu support.
+ */
+class MenuSupport implements \EP_Rules_Builder\RegistrationInterface {
+	/**
+	 * Determines if the object should be registered.
+	 *
+	 * @return bool True if the object should be registered, false otherwise.
+	 */
+	public function can_register() {
+		return is_admin();
+	}
+
+	/**
+	 * Registration method for the object.
+	 *
+	 * @return void
+	 */
+	public function register() {
+		add_submenu_page(
+			'elasticpress',
+			esc_html__( 'EP Rules Builder', 'ep-rules-builder' ),
+			esc_html__( 'EP Rules Builder', 'ep-rules-builder' ),
+			'manage_options',
+			'ep-rules-builder',
+			[ $this, 'screen_options' ]
+		);
+	}
+
+	/**
+	 * Screen options for the rules builder.
+	 *
+	 * @return void
+	 */
+	public function screen_options() {
+		echo \EP_Rules_Builder\include_template( 'options.php' ); // @codingStandardsIgnoreLine
+	}
+}
