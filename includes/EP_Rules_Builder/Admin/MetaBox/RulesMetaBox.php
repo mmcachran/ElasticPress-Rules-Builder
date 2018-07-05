7c721c99ec9cad98449128bdbe677fccc44412f3
mmcachran <mattmcachran@gmail.com>
2018-07-05 13:17:26 -0400

    Start of metabox.

--- includes/EP_Rules_Builder/Admin/MetaBox/RulesMetaBox.php
+++ includes/EP_Rules_Builder/Admin/MetaBox/RulesMetaBox.php
@@ -58,6 +58,108 @@ class RulesMetaBox extends AbstractMetaBox {
 	 * @return void
 	 */
 	public function get_metabox() {
-		// Initialize metabox here...
+		$fm = new \Fieldmanager_Group( [
+			'name'        => $this->get_metabox_name(),
+			'children'    => [
+
+				'condition' => new \Fieldmanager_Select(
+					[
+						'label'   => esc_html__( 'Condition', 'ep-rules-builder' ),
+						'options' => [
+							'any' => __( 'Any (or)', 'ep-rules-builder' ),
+							'all' => __( 'All (and)', 'ep-rules-builder' ),
+						],
+					]
+				),
+
+				'triggers'  => $this->get_triggers_metabox(),
+			],
+		] );
+
+		// Add the metabox.
+		$fm->add_meta_box( esc_html__( 'Rules', 'ep-rules-builder' ), $this->get_post_types() );
+	}
+
+	/**
+	 * Returns the triggers metabox group.
+	 *
+	 * @return \Fieldmanager_Group The triggers metabox group.
+	 */
+	protected function get_triggers_metabox() {
+		return new \Fieldmanager_Group( [
+			'label'          => esc_html__( 'Triggers', 'ep-rules-builder' ),
+			'label_macro'    => array( 'Trigger: %s', 'title' ),
+			'add_more_label' => esc_html__( 'Add Another Trigger', 'ep-rules-builder' ),
+			'limit'          => 0,
+			'sortable'       => true,
+			'collapsible'    => true,
+			'extra_elements' => 0,
+			'children'    => [
+
+				'operator' => new \Fieldmanager_Select(
+					[
+						'label'   => esc_html__( 'Operator', 'ep-rules-builder' ),
+						'options' => $this->get_operator_options(),
+					]
+				),
+			],
+		] );
+	}
+
+	/**
+	 * Get all operators
+	 *
+	 * @return array  operator options
+	 */
+	public function get_operator_options() {
+		// get numeric operators.
+		$numeric_operators = $this->get_numeric_operator_options();
+
+		// get string operators.
+		$string_operators = $this->get_string_operator_options();
+
+		// combine both sets.
+		$operators = array_merge( $numeric_operators, $string_operators );
+
+		/**
+		 * Filter allowed operators
+		 *
+		 * Turn on/off allowed operators.
+		 *
+		 * @param         array built-in operators
+		 */
+		return apply_filters( 'ep_rules_builder_operator_options', $operators );
+	}
+
+	/**
+	 * Get numeric operator
+	 *
+	 * @return array  operator options
+	 */
+	public function get_numeric_operator_options() {
+		return [
+			'equals' 				 => esc_html__( 'Equals', 'ep-rules-builder' ),
+			'does_not_equal' 		 => esc_html__( 'Does not equal', 'ep-rules-builder' ),
+			'equals_or_greater_than' => esc_html__( 'Equals or greater than', 'ep-rules-builder' ),
+			'equals_or_less_than' 	 => esc_html__( 'Equals or less than', 'ep-rules-builder' ),
+			'greater_than' 			 => esc_html__( 'Greater than', 'ep-rules-builder' ),
+			'less_than' 			 => esc_html__( 'Less than', 'ep-rules-builder' ),
+		];
+	}
+
+	/**
+	 * Get string operators
+	 *
+	 * @return array  operator options
+	 */
+	public function get_string_operator_options() {
+		return [
+			'contains' 			=> esc_html__( 'Contains', 'ep-rules-builder' ),
+			'does_not_contain'  => esc_html__( 'Does not contain', 'ep-rules-builder' ),
+			'is' 				=> esc_html__( 'Is', 'ep-rules-builder' ),
+			'is_not' 			=> esc_html__( 'Is not', 'ep-rules-builder' ),
+			'is_in' 			=> esc_html__( 'Is in', 'ep-rules-builder' ),
+			'is_not_in' 		=> esc_html__( 'Is not in', 'ep-rules-builder' ),
+		];
 	}
 }
